c7d81559d56f57cfa69e803913c735e423e5e4eb
WarCrazhed <mariozamora5126@gmail.com>
2022-11-25 16:22:03 -0600

    Vistas

--- app/src/main/java/com/example/proyecto10mo/objects/VariablesGlobales.kt
+++ app/src/main/java/com/example/proyecto10mo/objects/VariablesGlobales.kt
@@ -1,7 +1,7 @@
 package com.example.proyecto10mo.objects
 
 object VariablesGlobales {
-    val url = "http://192.168.1.9:8080/"
+    val url = "http://192.168.1.8:8080/"
     var idUsuario: String = ""
     var rol: String = ""
     var idEvento: String = ""
